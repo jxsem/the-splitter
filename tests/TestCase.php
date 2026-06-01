624157ec34be28e346f20636f42e06da4e676989
Jose Manuel <josemanuel.soldado@outlook.es>
2026-06-01 20:54:03 +0200

    docs(phpdoc): Agregar comentarios PHPDoc a clase base de tests
    - Documentación para TestCase base
    - Clase abstracta para tests de feature y unit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,12 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * @class TestCase
+ * @package Tests
+ * @description Clase base abstracta para todos los test de la aplicación.
+ * Proporciona utilidades y configuración común para tests de feature y unit.
+ */
 abstract class TestCase extends BaseTestCase
 {
     //
